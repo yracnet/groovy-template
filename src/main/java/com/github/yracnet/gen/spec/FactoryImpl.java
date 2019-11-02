3a01d4a010a9f447aed223cc0b346a6dd5715e90
Willyams Yujra <yracnet@gmail.com>
2019-11-02 18:27:34 -0400

    * Change Factory

--- src/main/java/com/github/yracnet/gen/spec/FactoryImpl.java
+++ src/main/java/com/github/yracnet/gen/spec/FactoryImpl.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.github.yracnet.gen.spec;
 
 import java.util.HashMap;
@@ -17,10 +16,8 @@ public class FactoryImpl extends FactoryAbstract {
 
     public Map createReactPage(String name) {
         Map map = new HashMap();
-        name = toName(name);
         String nameSufix = upperFirst(name);
-        //String varSufix = toVar(nameSufix);
-        map.put("type", nameSufix + "Page");
+        map.put("name", nameSufix + "Page");
         map.put("create", nameSufix + "Create");
         map.put("update", nameSufix + "Update");
         map.put("remove", nameSufix + "Remove");
@@ -29,7 +26,21 @@ public class FactoryImpl extends FactoryAbstract {
         map.put("proc01", nameSufix + "Proc01");
         map.put("inbox", nameSufix + "Inbox");
         map.put("crud", nameSufix + "CRUD");
-        map.put("path", toPath(nameSufix)+"/page");
+        map.put("path", toPath(nameSufix) + "/page");
+        return map;
+    }
+
+    public Map createReactPage(String name, String classifier) {
+        Map map = new HashMap();
+        name = toName(name + classifier);
+        String nameSufix = upperFirst(name);
+        map.put("name", nameSufix);
+        map.put("local", nameSufix + "Local");
+        map.put("state", nameSufix + "State");
+        map.put("props", nameSufix + "Props");
+        map.put("event", nameSufix + "Event");
+        map.put("path", toPath(nameSufix) + "/page");
+        map.put("url", toPath(nameSufix));
         return map;
     }
 
@@ -37,13 +48,25 @@ public class FactoryImpl extends FactoryAbstract {
         Map map = new HashMap();
         name = toName(name);
         String nameSufix = upperFirst(name);
-        //String varSufix = toVar(nameSufix);
-        map.put("type", nameSufix + "Part");
+        map.put("name", nameSufix + "Part");
         map.put("form", nameSufix + "Form");
         map.put("view", nameSufix + "View");
         map.put("query", nameSufix + "Query");
         map.put("table", nameSufix + "Table");
-        map.put("path", toPath(nameSufix)+"/part");
+        map.put("path", toPath(nameSufix) + "/part");
+        return map;
+    }
+
+    public Map createReactPart(String name, String classifier) {
+        Map map = new HashMap();
+        name = toName(name + classifier);
+        String nameSufix = upperFirst(name);
+        map.put("name", nameSufix);
+        map.put("local", nameSufix + "Local");
+        map.put("state", nameSufix + "State");
+        map.put("props", nameSufix + "Props");
+        map.put("event", nameSufix + "Event");
+        map.put("path", toPath(nameSufix) + "/part");
         return map;
     }
 
@@ -51,12 +74,28 @@ public class FactoryImpl extends FactoryAbstract {
         Map map = new HashMap();
         name = toName(name);
         String nameSufix = upperFirst(name);
-        //String varSufix = toVar(nameSufix);
-        map.put("type", nameSufix + "Model");
+        map.put("name", nameSufix + "Model");
+        map.put("text", toText(nameSufix) );
         map.put("data", nameSufix);
         map.put("filter", nameSufix + "Filter");
         map.put("message", nameSufix + "Message");
         map.put("validate", nameSufix + "Validate");
+        map.put("path", toPath(nameSufix));
+        return map;
+    }
+
+    public Map createReactRest(String name) {
+        Map map = new HashMap();
+        name = toName(name);
+        String nameSufix = upperFirst(name);
+        String varName = lowerFirst(name);
+        map.put("name", nameSufix + "Rest");
+        map.put("ping", varName);
+        map.put("create", varName + "Create");
+        map.put("update", varName + "Update");
+        map.put("remove", varName + "Remove");
+        map.put("filter", varName + "Filter");
+        map.put("query", varName + "Query");
         map.put("path", toPath(nameSufix));
         return map;
     }
